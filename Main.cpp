2865c4f3ff7a853fd1274da8f10710274248fc62
AlanoSong <AlanoSong@163.com>
2023-05-19 19:59:19 +0800

    refine proj: add qt and cmd build config

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,15 @@
-#include <QtWidgets/QApplication>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <tchar.h>
 
-#include "./MiniThing/Qt/MiniThingQt.h"
 #include "./MiniThing/Core/MiniThingCore.h"
+
+#ifdef BUILD_FOR_QT
+
+#include <QtWidgets/QApplication>
+#include "./MiniThing/Qt/MiniThingQt.h"
 
 int main(int argc, char *argv[])
 {
@@ -12,3 +20,42 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
+#else // BUILD_FOR_QT
+
+int main(int argc, char* argv[])
+{
+    LARGE_INTEGER timeStart;
+    LARGE_INTEGER timeEnd;
+    LARGE_INTEGER frequency;
+    QueryPerformanceFrequency(&frequency);
+    double quadpart = (double)frequency.QuadPart;
+    QueryPerformanceCounter(&timeStart);
+
+    MiniThingCore* pMiniThingCore = new MiniThingCore(".\\MiniThing.db");
+    pMiniThingCore->StartInstance();
+
+    QueryPerformanceCounter(&timeEnd);
+    double elapsed = (timeEnd.QuadPart - timeStart.QuadPart) / quadpart;
+    printf_s("Time elasped: %f S\n", elapsed);
+
+    if (FAILED(pMiniThingCore->CreateMonitorThread()))
+    {
+        assert(0);
+    }
+    if (FAILED(pMiniThingCore->CreateQueryThread()))
+    {
+        assert(0);
+    }
+    pMiniThingCore->StartMonitorThread();
+    pMiniThingCore->StartQueryThread();
+
+    Sleep(1000 * 1200);
+
+    pMiniThingCore->StopMonitorThread();
+    pMiniThingCore->StopQueryThread();
+
+    return 0;
+}
+
+#endif // BUILD_FOR_QT
